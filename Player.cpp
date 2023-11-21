ddce5c4a9398c4a9027354760c1cba682fe578d3
Erika Schaible <schaible@mcmaster.ca>
2023-11-21 09:12:05 -0500

    Git push share attempt

--- Player.cpp
+++ Player.cpp
@@ -7,6 +7,7 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos.setObjPos(mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, '*');
 }
 
 
@@ -17,16 +18,101 @@ Player::~Player()
 
 void Player::getPlayerPos(objPos &returnPos)
 {
+    returnPos.setObjPos(playerPos.x, playerPos.y, playerPos.symbol);
     // return the reference to the playerPos arrray list
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic 
+
+    //Where does the input come from? How to check for input?
+    //Hint not MacUILib get char
+    //There will be a method in gamemechanism class that gets input and stores most recent
+    //Just need to figure out how to get to it
+    //How? It lies within the Gamemechs* inside your private member
+    char input = mainGameMechsRef->getInput();
+    switch(input)
+    {
+        case 'w':
+                if (myDir != DOWN && myDir != UP)
+                {
+                    myDir = UP;
+                }
+                break;
+
+            case 'a':
+                if (myDir != RIGHT && myDir != LEFT)
+                {
+                    myDir = LEFT;
+                }
+                break;
+                
+
+            case 's':
+                if (myDir != UP && myDir != DOWN)
+                {
+                    myDir = DOWN;
+                }
+                break;
+                
+            case 'd':
+                if (myDir != RIGHT && myDir != LEFT)
+                {
+                    myDir = RIGHT;
+                }
+                break;
+
+            default:
+                break;
+    }       
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    //Work in progress
+    //pretty sure the logic for how to move the player needs to be updated
+    
+    //playerPos.getObjPos(); //does this work?
+    switch(myDir)
+    {
+        case UP:
+            //playerPos.setObjPos() something like this...maybe sorta
+            playerPos.y = playerPos.y - 1;
+            if (playerPos.y == 0)
+            {
+                //playerPos.y = mainGameMechsRef->getBoardSizeY() - 2; //error here
+            }
+            break;
+
+        case DOWN:
+            playerPos.y++;
+            if (playerPos.y == 9)//playerPos.y == mainGameMechsRef->getBoardSizeY() - 1) //same error here
+            {
+                playerPos.y = 1;
+            }
+            break;
+
+        case LEFT:
+            playerPos.x--;
+            if (playerPos.x == 0)
+            {
+                //playerPos.x = mainGameMechsRef->getBoardSizeX() - 2;
+            }
+            break;
+
+        case RIGHT:
+            playerPos.x++;
+            if (playerPos.x == 9)//playerPos.x == mainGameMechsRef->getBoardSizeY() - 1)
+            {
+                playerPos.x = 1;
+            }
+            break;
+
+        default:
+            break;
+    }
+
 }
 
--- Project.cpp
+++ Project.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
+#include "Player.h"
 
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+objPos myPos;
+
+GameMechs* myGM;
+Player* myPlayer;
 
 void Initialize(void);
 void GetInput(void);
@@ -23,7 +28,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(myGM->getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -41,7 +46,9 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    myGM = new GameMechs(26, 13); //makes board size 26 x 13
+    myPlayer = new Player(myGM);
+
 }
 
 void GetInput(void)
@@ -51,13 +58,59 @@ void GetInput(void)
 
 void RunLogic(void)
 {
-    
+    myPlayer->updatePlayerDir();
 }
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen(); 
+    int row, col, k;
+    //error with the mainGameMechsRef->, doesn't work....
 
+    // for (row = 0; row < mainGameMechsRef->getBoardSizeY(); row ++)
+    // {
+    //     if (row == 0 || row == 9)
+    //     {
+    //         MacUILib_printf("####################");
+    //     }
+
+    //     else
+    //     {
+    //         for (col = 0; col < mainGameMechsRef->getBoardSizeX(); col++)
+    //         {
+    //             if (col == 0 || col == 19)
+    //             {
+    //                 MacUILib_printf("#");
+    //             }
+    //             // else if (row == playerPos.y && col == playerPos.x)//do a get object pos here
+    //             // {
+    //             //     MacUILib_printf("%c", playerPos.symbol);
+    //             // }
+    //             else
+    //             {
+    //                 //int isOccupied = 0;
+    //                 // for(k = 0; k < 5; k++)
+    //                 // {
+    //                 //     if (row == itemBin[k].y && col == itemBin[k].x)
+    //                 //     {
+    //                 //         MacUILib_printf("%c", itemBin[k].symbol);
+    //                 //         isOccupied = 1;
+    //                 //         break;
+    //                 //     }
+    //                 // }
+    //                 //if(isOccupied == 0)
+    //                 //{
+    //                     MacUILib_printf(" ");
+    //                 //}
+    //             }
+    //         }
+    //     }   
+    //     MacUILib_printf("\n");
+
+    //}
+    objPos tempPos;
+    myPlayer->getPlayerPos(tempPos);
+    MacUILib_printf("Board Size: %d x %d, Player Pos: <%d, %d> + %c\n", myGM->getBoardSizeX(), myGM->getBoardSizeY(), tempPos.x, tempPos.y, tempPos.symbol);
 }
 
 void LoopDelay(void)
